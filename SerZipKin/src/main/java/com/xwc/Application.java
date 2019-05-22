9caa35365e88dc6cfa26356476ee485083110bd1
xuweichao <xuweichao010@163.com>
2019-05-22 21:34:14 +0800

    监控中心

---
+++ SerZipKin/src/main/java/com/xwc/Application.java
@@ -0,0 +1,23 @@
+package com.xwc;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 创建人：徐卫超
+ * 创建时间：2019/5/8  14:09
+ * 业务：
+ * 功能：
+ */
+@EnableEurekaClient
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class)
+                .web(WebApplicationType.SERVLET).run(args);
+    }
+}
