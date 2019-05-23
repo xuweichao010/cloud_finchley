f6d5d1c064bb5e5a5ac4fa9065bb42c719632440
xuweichao <xuweichao010@163.com>
2019-05-23 12:58:26 +0800

    配置中心管理注册中心配置

--- SerZipKin/src/main/java/com/xwc/Application.java
+++ SerZipKin/src/main/java/com/xwc/Application.java
@@ -3,9 +3,7 @@ package com.xwc;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import zipkin.server.internal.EnableZipkinServer;
 
 /**
  * 创建人：徐卫超
@@ -15,7 +13,6 @@ import zipkin.server.internal.EnableZipkinServer;
  */
 @EnableEurekaClient
 @SpringBootApplication
-@EnableZipkinServer
 public class Application {
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class)
